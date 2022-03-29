ad5ffa20cb7fddcc1bafb4df67d90591379b02c5
AIPERIM AKISHEVA <AIPERIM1995@GMAIL.COM>
2022-03-28 21:54:58 -0400

    created Relationship Graphs in core data implement uisearchbar and querying with core data

---
+++ Todoey/Controllers/CategoryViewControllerTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CategoryViewControllerTableViewController.swift
+//  Todoey
+//
+//  Created by Aiperim Akisheva on 3/28/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+class CategoryViewControllerTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - TableView Datasource Methods
+    
+    // MARK: - Data Manipulation Mathods
+   
+    // MARK: - Add New Categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+   
+    
+    
+    
+    // MARK: - TableView Delegate Mathods
+    
+    
+    
+    
+}
